ccbf031865a30a62fd23ebbc0f98c1d363bd4789
Deborah-Chen <dsc001@ucsd.edu>
2023-05-25 11:41:22 -0700

    class05

---
+++ class05/class04 copy.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "Plots from Week 2"
+
+#' author: "Patricia Chen"
+
+#' date: "April 18th, 2023"
+
+#' ---
+
+#a plot from week 2
+x <- 1:50
+
+plot(x, sin(x))
+
+#make this a bit nicer
+plot(x, sin(x), type="l", col="blue", lwd=2)
---
+++ class05/class04.R
@@ -0,0 +1,14 @@
+#' ---
+
+#' title: "Plots"
+#' author: "A16125573"
+
+#' ---
+
+# a plot from week 2
+
+x <- 1:50
+
+plot(x, sin(x))
+
+plot(x, sin(x), type="l", col="lightblue", lwd=2)
